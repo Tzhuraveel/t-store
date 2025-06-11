4282cdad088081d54ffa0fdcb2ab89b3e3a62736
Tymofii Zhuravel <timofiyzhuravel2003@gmail.com>
2025-06-11 17:42:02 +0300

    feat(health): add Health module and controller with heartbeat endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { DatabaseModule } from '#infra/database/database.module';
+import { HealthModule } from '#modules/health/health.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, HealthModule],
 })
 export class AppModule {}
